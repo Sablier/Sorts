fd35c852f8b88e8d2ec2830c9cdd825e9537b4a8
sablier <mrsablier@163.com>
2019-06-15 00:05:41 +0800

    添加注释

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,12 +1,18 @@
 def selection_sort(tlist):
+    # 共执行n-1轮选择
     n = len(tlist) - 1
+
     while n > 0:
+        # 取出基准
         pivot = tlist.pop(0)
+        # 便利列表，如果找到比基准小的值则和基准交换
         for i in range(n):
             if tlist[i] < pivot:
                 pivot, tlist[i] = tlist[i], pivot
+        # 基准值为最小值，将其添加到最后
         tlist.append(pivot)
         n -= 1
+    # 循环完毕后还剩一个值，这是最大值，要添加到列表最后
     tlist.append(tlist.pop(0))
     return tlist
 
