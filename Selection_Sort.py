4185781c9216105899e07c10e3e4206b649291e2
sablier <albertpeng1991@gmail.com>
2019-09-02 15:41:11 +0800

    更新和修补

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -5,7 +5,7 @@ def selection_sort(tlist):
     while n > 0:
         # 取出基准
         pivot = tlist.pop(0)
-        # 便利列表，如果找到比基准小的值则和基准交换
+        # 遍历列表，如果找到比基准小的值则和基准交换
         for i in range(n):
             if tlist[i] < pivot:
                 pivot, tlist[i] = tlist[i], pivot
@@ -14,7 +14,29 @@ def selection_sort(tlist):
         n -= 1
     # 循环完毕后还剩一个值，这是最大值，要添加到列表最后
     tlist.append(tlist.pop(0))
+
     return tlist
+
+
+def select_sort(li):
+
+    n = len(li)
+
+    # 选择的次数，n-1次，每一次选择一个最小的数，依次放到0~ n-1 位置
+    for j in range(n-1):
+
+        # 选择一次，把最小的数放到0位置
+        # 认为0位置的数据最小，记录0位置
+        min_index = j
+        # 那最小的数，跟后面所有的数据进行比较
+        for i in range(1+j, n):
+            # 拿最小的数跟当前位置的数比较，让min_index 记录最小的数的位置
+            if li[i] < li[min_index]:
+                min_index = i
+        # for循环结束，min_index指向最小的数
+        li[min_index], li[j] = li[j], li[min_index]
+
+    return li
 
 
 # 逻辑代码11行
@@ -22,3 +44,4 @@ if __name__ == '__main__':
     tlist = [17138, 15118, 13561, 14307, 19857, 19439, 12375, 13472, 15055, 18156, 19527, 12162, 9156, 10523, 19680,
              18237, 9967, 15655, 15478, 11036]
     print(selection_sort(tlist))
+    print(select_sort(tlist))
