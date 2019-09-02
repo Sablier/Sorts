d8056e2401808c8cd639f701cdcbd371c9642cf9
sablier <albertpeng1991@gmail.com>
2019-09-02 15:14:04 +0800

    添加注释

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -11,6 +11,14 @@ def bubble(context):
     print("the new array is %s " % context)
 
 
+def bubble_sort(alist):
+    for j in range(len(alist)-1,0,-1):
+        for i in range(j):
+            if alist[i] > alist[i+1]:
+                alist[i], alist[i+1] = alist[i+1], alist[i]
+
+
 if __name__ == '__main__':
     context = [18422, 15669, 9854, 12705, 18350, 19424, 9072, 14817, 11030, 16525]
     bubble(context)
+    bubble_sort(context)
